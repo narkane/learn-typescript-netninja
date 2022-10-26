f530b6a1912298fe8950dc792055a32825f177e7
Narkane <enikath@gmail.com>
2022-10-26 18:34:39 -0500

    strict typing example

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,7 @@ var inputs = document.querySelectorAll("input");
 inputs.forEach(function (input) {
     console.log(input);
 });
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(3));
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,9 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
   console.log(input);
 });
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(3));
